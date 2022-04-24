a31c83d35c9c35029b56be995fcbb2d31815131d
AndreaCuriel <acurielipn@hotmail.com>
2022-04-24 13:34:34 -0500

    Sprint 3: Requerimiento 3. Prueba exitosa. Missing properties

--- twitter/test/views/UserView.test.js
+++ twitter/test/views/UserView.test.js
@@ -1,3 +1,4 @@
+const User = require("../../app/models/user");
 const UserView = require("./../../app/views/UserView");
 
 describe("Tests for UserView", () => {
@@ -10,6 +11,12 @@ describe("Tests for UserView", () => {
 
   test("Return an error object when try to create a new user with a playload with invalid properties", () => {
     const payload = { username: null, name: 12, id: "id" };
+    const result = UserView.createUser(payload);
+    expect(result.error).toMatch(/Necesitan tener un valor válido/);
+  });
+
+  test("Return an error objet when try to creare a new user with a payload with missing properties", () => {
+    const payload = { username: "Username" };
     const result = UserView.createUser(payload);
     expect(result.error).toMatch(/Necesitan tener un valor válido/);
   });
