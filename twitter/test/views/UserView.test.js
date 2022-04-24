7c9335692147982a0015d0c80a82b00ee4f5a1f9
AndreaCuriel <acurielipn@hotmail.com>
2022-04-24 13:04:05 -0500

    Sprint 3: 5) Requerimiento 2, Prueba fallida. Invalid properties

--- twitter/test/views/UserView.test.js
+++ twitter/test/views/UserView.test.js
@@ -7,4 +7,10 @@ describe("Tests for UserView", () => {
     //htpp://jestjs.io/docs/using-matchers#strings
     expect(result.error).toMatch(/payload no existe/);
   });
+
+  test("Return an error object when try to create a new user with a playload with invalid properties", () => {
+    const payload = { username: null, name: 12, id: "id" };
+    const result = UserView.createUser(payload);
+    expect(result.error).toMatch(/necesitan tener un valor valido/);
+  });
 });
