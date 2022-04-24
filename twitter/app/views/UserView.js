1f171d45cd04baded39e26d35275e21e53589ed3
AndreaCuriel <acurielipn@hotmail.com>
2022-04-24 13:47:27 -0500

    Sprint 3: Requerimiento 4. Prueba exitosa. valid payload

--- twitter/app/views/UserView.js
+++ twitter/app/views/UserView.js
@@ -3,9 +3,18 @@ const UserService = require("./../services/UserService");
 class UserView {
   static createUser(payload) {
     if (payload === null) {
-      ///if the value is null, send error
+      ///1) if the value is null, send error
       console.log("Error es null");
       return { error: "El payload no existe." };
+      ///2) ivalid properties
+    } else if (
+      typeof payload.username != "string" ||
+      typeof payload.name != "string" ||
+      typeof payload.id != "number"
+    ) {
+      return { error: "Necesitan tener un valor válido" };
+    } else {
+      return UserService.create(payload.id, payload.username, payload.name);
     }
   }
 }
